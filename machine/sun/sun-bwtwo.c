8fa11896846b5cb2af592632cc355c80ad807ca6
rooster <rooster@sibs2>
2014-11-22 02:58:38 -0600

    NetBSD pkgsrc patch-af

--- machine/sun/sun-bwtwo.c
+++ machine/sun/sun-bwtwo.c
@@ -339,7 +339,8 @@ tme_sun_bwtwo(struct tme_element *element, const char * const *args, char **_out
 {
   struct tme_sunbw2 *sunbw2;
   int rc;
-
+  tme_uint16_t v;
+  
   /* start the sunbw2 structure: */
   sunbw2 = tme_new0(struct tme_sunbw2, 1);
   sunbw2->tme_sunbw2_sunfb.tme_sunfb_element = element;
@@ -364,12 +365,13 @@ tme_sun_bwtwo(struct tme_element *element, const char * const *args, char **_out
   case TME_SUNBW2_TYPE_MULTIBUS:
   case TME_SUNBW2_TYPE_OLD_ONBOARD:
 
-    /* set our initial CSR: */
-    sunbw2->tme_sunbw2_csr
-      = tme_htobe_u16(TME_SUNBW2_CSR_ENABLE_VIDEO
+    v = TME_SUNBW2_CSR_ENABLE_VIDEO
 		      | (sunbw2->tme_sunbw2_sunfb.tme_sunfb_size == TME_SUNFB_SIZE_1024_1024
 			 ? TME_SUNBW2_CSR_JUMPER_HIRES
-			 : 0));
+			 : 0);
+    /* set our initial CSR: */
+    sunbw2->tme_sunbw2_csr
+      = tme_htobe_u16(v);
 
     break;
   }
