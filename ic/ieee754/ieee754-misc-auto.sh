255faf4225f4c07d2367cd3ba127761dbba37b2f
rooster <rooster@sibs2>
2014-11-22 03:17:09 -0600

    NetBSD patch

--- ic/ieee754/ieee754-misc-auto.sh
+++ ic/ieee754/ieee754-misc-auto.sh
@@ -106,7 +106,7 @@ typedef float tme_ieee754_${precision}_builtin_t;
 #elif (TME_FLOAT_FORMAT_IEEE754_${capprecision}_BUILTIN == TME_FLOAT_FORMAT_DOUBLE)
 typedef double tme_ieee754_${precision}_builtin_t;
 #define tme_float_value_ieee754_${precision}_builtin tme_float_value_double
-#elif (TME_FLOAT_FORMAT_IEEE754_${capprecision}_BUILTIN == TME_FLOAT_FORMAT_LONG_DOUBLE)
+#elif (defined(_TME_HAVE_LONG_DOUBLE) && (TME_FLOAT_FORMAT_IEEE754_${capprecision}_BUILTIN == TME_FLOAT_FORMAT_LONG_DOUBLE))
 typedef long double tme_ieee754_${precision}_builtin_t;
 #define tme_float_value_ieee754_${precision}_builtin tme_float_value_long_double
 #endif
