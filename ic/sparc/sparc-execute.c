dc6bbb6225bbb620afccdf29fd5cc339a49e07db
rooster <rooster@sibs2>
2014-11-22 02:51:40 -0600

    NetBSD pkgsrc patch-ab

--- ic/sparc/sparc-execute.c
+++ ic/sparc/sparc-execute.c
@@ -612,7 +612,7 @@ _TME_SPARC_EXECUTE_NAME(struct tme_sparc *ic)
 			    != pc_previous)) {
       if (__tme_predict_true(((tme_sparc_ireg_t) (pc + sizeof(tme_uint32_t)))
 			     == ic->tme_sparc_ireg(TME_SPARC_IREG_PC_NEXT))) {
-	tme_recode_thunk_off_t insns_thunk;
+	tme_recode_thunk_off_t insns_thunk = 0;
 
 	/* if the current instruction TLB entry is not the invalid TLB
 	   entry, and there is an instructions thunk for this PC: */
