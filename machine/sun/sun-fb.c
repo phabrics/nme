19a60bf6cb3697c49aca2671e3836b15e97f3054
phabrics <phabrics@phabrics.com>
2015-03-27 05:47:48 -0500

    Removed mutex from sun fb tlb fill to undo deadlock.  Added new thread headers to distribution.

--- machine/sun/sun-fb.c
+++ machine/sun/sun-fb.c
@@ -985,7 +985,7 @@ _tme_sunfb_tlb_fill(void *_sunfb,
   assert (address == address_wider);
 
   /* lock the mutex: */
-  tme_mutex_lock(&sunfb->tme_sunfb_mutex);
+  //tme_mutex_lock(&sunfb->tme_sunfb_mutex);
 
   /* if this address falls in the bus subregion for memory: */
   if ((sunfb->tme_sunfb_bus_subregion_memory.tme_bus_subregion_address_first
@@ -1120,7 +1120,7 @@ _tme_sunfb_tlb_fill(void *_sunfb,
   tlb->tme_bus_tlb_cycle_private = _sunfb;
 
   /* unlock the mutex: */
-  tme_mutex_unlock(&sunfb->tme_sunfb_mutex);
+  //tme_mutex_unlock(&sunfb->tme_sunfb_mutex);
 
   return (TME_OK);
 }
