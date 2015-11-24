59a0f8d2961b30495f00ddce0c99d97bf7e5f7ed
Ruben Agin <phabrics@phabrics.com>
2015-11-24 08:40:44 +0000

    Added full options from openvpn to be able to configure devices completely.

--- host/openvpn/tap.c
+++ host/openvpn/tap.c
@@ -36,145 +36,87 @@
 /* includes: */
 #define OPENVPN_HOST
 #include "eth-impl.c"
+#include "options.h"
 
 /* the new TAP function: */
 TME_ELEMENT_SUB_NEW_DECL(tme_host_openvpn,tap) {
   int rc;
   struct gc_arena gc = gc_new ();
-  const char *dev, *dev_type, *dev_node, *dev_addr;
-  const char *inet, *netmask, *inet6, *remote6, *netbits;
   unsigned char *hwaddr;
   unsigned int hwaddr_len;
   struct tuntap *tt;
   int arg_i;
-  int usage;
   struct frame frame;
+  struct env_set *es;
+  struct options options;       /**< Options loaded from command line or
+                                 *   configuration file. */
   
-  /* check our arguments: */
-  usage = 0;
+  arg_i = 0;
 
-  dev = dev_type = dev_node = dev_addr =
-    inet = netmask = inet6 = remote6 = netbits = NULL;
-  
-  arg_i = 1;
+  while(args[++arg_i] != NULL);
 
-  for (;;) {
-    /* the interface we're supposed to use: */
-    if (TME_ARG_IS(args[arg_i + 0], "dev")
-	&& args[arg_i + 1] != NULL) {
-      dev = args[arg_i + 1];
-    }
-
-    else if (TME_ARG_IS(args[arg_i + 0], "dev-type")
-	&& args[arg_i + 1] != NULL) {
-      dev_type = args[arg_i + 1];
-    }
-
-    else if (TME_ARG_IS(args[arg_i + 0], "dev-node")
-	&& args[arg_i + 1] != NULL) {
-      dev_node = args[arg_i + 1];
-    }
-
-    else if (TME_ARG_IS(args[arg_i + 0], "dev-addr")
-	&& args[arg_i + 1] != NULL) {
-      dev_addr = args[arg_i + 1];
-    }
-
-    else if(TME_ARG_IS(args[arg_i + 0], "inet") 
-	 && args[arg_i + 1] != NULL) {
-      inet = args[arg_i + 1];
-    }
-
-    else if (TME_ARG_IS(args[arg_i + 0], "netmask")
-	&& args[arg_i + 1] != NULL) {
-      netmask =  args[arg_i + 1];
-    }
-
-    else if(TME_ARG_IS(args[arg_i + 0], "inet6") 
-	 && args[arg_i + 1] != NULL) {
-      inet6 = args[arg_i + 1];
-    }
-
-    else if(TME_ARG_IS(args[arg_i + 0], "remote6") 
-	 && args[arg_i + 1] != NULL) {
-      remote6 = args[arg_i + 1];
-    }
-
-    else if (TME_ARG_IS(args[arg_i + 0], "prefixlen")
-	&& args[arg_i + 1] != NULL) {
-      netbits =  args[arg_i + 1];
-    }
-
-    /* if we ran out of arguments: */
-    else if (args[arg_i + 0] == NULL) {
-      break;
-    }
-
-    /* otherwise this is a bad argument: */
-    else {
-      tme_output_append_error(_output,
-			      "%s %s", 
-			      args[arg_i],
-			      _("unexpected"));
-      usage = TRUE;
-      break;
-    }
-    arg_i += 2;
-  }
-
-  if (usage) {
-    tme_output_append_error(_output,
-			    "%s %s [ dev %s ] [ dev-type %s ] [ dev-node %s ] [ dev-addr %s ] [ inet %s ] [ netmask %s ] [ inet6 %s] [remote6 %s]",
-			    _("usage:"),
-			    args[0],
-			    _("NAME"),
-			    _("TYPE"),
-			    _("NODE"),
-			    _("LLADDR"),
-			    _("IPADDRESS"),
-			    _("IPADDRESS"),
-			    _("IP6ADDRESS"),
-			    _("IP6ADDRESS"));
-    return (EINVAL);
-  }
-  
-  if(!dev) dev = "tap";
-  
-  tme_log(&element->tme_element_log_handle, 0, TME_OK, 
-	  (&element->tme_element_log_handle, 
-	   "trying tap device (%s, type %s, node %s) with addresses: link %s, ip %s/%s, ip6 %s/%s rem6 %s",
-	   dev,
-	   (dev_type) ? (dev_type) : "",
-	   (dev_node) ? (dev_node) : "",
-	   (dev_addr) ? (dev_addr) : "",
-	   (inet) ? (inet) : "",
-	   (netmask) ? (netmask) : "",
-	   (inet6) ? (inet6) : "",
-	   (netbits) ? (netbits) : "",
-	   (remote6) ? (remote6) : ""));
-	  
   error_reset();
+
+  /* initialize environmental variable store */
+  es = env_set_create(NULL);
 #ifdef WIN32
   init_win32();
+  set_win_sys_path_via_env(es);
 #endif
-  tt = init_tun(dev,
-		dev_type,
-		TOP_SUBNET,
-		inet,
-		netmask,		
-		inet6,
-		(netbits) ? (atoi(netbits)) : (0),
-		remote6,
-		0,
-		0,
-		0,
-		NULL);
+
+  /* initialize options to default state */
+  init_options(&options, true);
+
+  /* parse command line options, and read configuration file */
+  parse_argv(&options, arg_i, args, M_USAGE, OPT_P_DEFAULT, NULL, es);
+
+  /* set dev options */
+  init_options_dev(&options);
+
+  /* tun/tap persist command? */
+  if(do_persist_tuntap(&options))
+    return (TME_OK);
+
+  /* sanity check on options */
+  options_postprocess(&options);
+
+  /* show all option settings */
+  show_settings(&options);
+
+  /* print version number */
+  //msg(M_INFO, "%s", title_string);
+  show_library_versions(M_INFO);
+
+  /* misc stuff */
+  pre_setup(&options);
+
+  /* set certain options as environmental variables */
+  setenv_settings(es, &options);
+
+  /* TUN/TAP specific stuff */
+  tt = init_tun(options.dev,
+		options.dev_type,
+		options.topology,
+		options.ifconfig_local,
+		options.ifconfig_remote_netmask,
+		options.ifconfig_ipv6_local,
+		options.ifconfig_ipv6_netbits,
+		options.ifconfig_ipv6_remote,
+		0, //addr_host (&c->c1.link_socket_addr.local),
+		0, //addr_host (&c->c1.link_socket_addr.remote),
+		!options.ifconfig_nowarn,
+		es);
 
   if(!tt) return (EINVAL);
 
   memset(&frame, 0, sizeof(frame));  
-  tun_adjust_frame_parameters(&frame, TAP_MTU_EXTRA_DEFAULT);
+  /*
+   * Adjust frame size based on the --tun-mtu-extra parameter.
+   */
+  if(options.ce.tun_mtu_extra_defined)
+    tun_adjust_frame_parameters(&frame, options.ce.tun_mtu_extra);
 
+  /* See frame_finalize_options (struct context *c, const struct options *o) */
   frame_align_to_extra_frame(&frame);
   frame_or_align_flags(&frame,
 		       FRAME_HEADROOM_MARKER_FRAGMENT
@@ -182,29 +124,24 @@ TME_ELEMENT_SUB_NEW_DECL(tme_host_openvpn,tap) {
 		       |FRAME_HEADROOM_MARKER_READ_STREAM);
   
   frame_finalize(&frame,
-		 false,
-		 LINK_MTU_DEFAULT,
-		 true,
-		 TUN_MTU_DEFAULT);
+		 options.ce.link_mtu_defined,
+		 options.ce.link_mtu,
+		 options.ce.tun_mtu_defined,
+		 options.ce.tun_mtu);
 
-#ifdef WIN32
-  overlapped_io_init (&tt->reads, &frame, FALSE, true);
-  overlapped_io_init (&tt->writes, &frame, TRUE, true);
-  tt->rw_handle.read = tt->reads.overlapped.hEvent;
-  tt->rw_handle.write = tt->writes.overlapped.hEvent;
-  tt->adapter_index = TUN_ADAPTER_INDEX_INVALID;
-#endif
-  
-  if(inet6) tt->ipv6 = TRUE;
+  /* flag tunnel for IPv6 config if --tun-ipv6 is set */
+  tt->ipv6 = options.tun_ipv6;
+
+  init_tun_post(tt, &frame, &options.tuntap_options);
   
   if(ifconfig_order() == IFCONFIG_BEFORE_TUN_OPEN) {
     /* guess actual tun/tap unit number that will be returned
        by open_tun */
-    const char *guess = guess_tuntap_dev(dev,
-					 dev_type,
-					 dev_node,
+    const char *guess = guess_tuntap_dev(options.dev,
+					 options.dev_type,
+					 options.dev_node,
 					 &gc);
-    do_ifconfig(tt, guess, TUN_MTU_SIZE(&frame), NULL);
+    do_ifconfig(tt, guess, TUN_MTU_SIZE(&frame), es);
   }
 
 #ifndef OPENVPN_ETH
@@ -212,20 +149,20 @@ TME_ELEMENT_SUB_NEW_DECL(tme_host_openvpn,tap) {
   tt->ipv6 = FALSE;
 #endif
   /* open the tun device */
-  open_tun(dev,
-	   dev_type,
-	   dev_node,
+  open_tun(options.dev,
+	   options.dev_type,
+	   options.dev_node,
 	   tt);
 
-  if(inet6) tt->ipv6 = TRUE;
+  tt->ipv6 = options.tun_ipv6;
 
   /* set the hardware address */
-  if(dev_addr)
-    set_lladdr(tt->actual_name, dev_addr, NULL);
+  if(options.lladdr)
+    set_lladdr(tt->actual_name, options.lladdr, es);
   
   /* do ifconfig */
   if(ifconfig_order() == IFCONFIG_AFTER_TUN_OPEN) {
-    do_ifconfig(tt, tt->actual_name, TUN_MTU_SIZE(&frame), NULL);
+    do_ifconfig(tt, tt->actual_name, TUN_MTU_SIZE(&frame), es);
   }
   
   /* find the interface we will use: */
