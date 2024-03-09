c926faed883e4674a95757d01d4b205f5cfda67d
phabrics <phabrics@phabrics.com>
2024-03-09 06:42:03 -0600

    Added <libopenvpn/lib/mman.h> into appropriate include path.

--- libtme/host/recode-mmap.c
+++ libtme/host/recode-mmap.c
@@ -46,7 +46,7 @@ _TME_RCSID("$Id: recode-mmap.c,v 1.2 2008/07/01 02:00:53 fredette Exp $");
 #ifdef HAVE_SYS_MMAN_H
 #include <sys/mman.h>
 #else
-#include "mman.h"
+#include <libopenvpn/lib/mman.h>
 #endif
 #ifdef WIN32
 #include <windows.h>
